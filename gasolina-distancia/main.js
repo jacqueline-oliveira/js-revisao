9ad1d8ec70769d3d7d56d0a0922407930df79e7c
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-02-07 19:49:00 -0300

    implementação método gasolina

--- gasolina-distancia/main.js
+++ gasolina-distancia/main.js
@@ -1,3 +1,13 @@
 function calcularGasolina() {
+  let distancia = document.getElementById('distancia').value;
+  let consumo = document.getElementById('consumo').value;
+  let preco = document.getElementById('preco').value;
 
+  let quantidadeGasolina = distancia / consumo;
+  let custoViagem = quantidadeGasolina * preco;
+
+  let resultaLitros = document.querySelector("#resultado_gasolina_litros"); 
+  resultaLitros.innerHTML = quantidadeGasolina.toFixed(2);
+  let resultadoCusto = document.querySelector("#resultado_custo");
+  resultadoCusto.innerHTML = `R$ ${custoViagem.toFixed(2)}`;
 }
