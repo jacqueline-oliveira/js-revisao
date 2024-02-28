08e2c7f454fb0340a0b7f94fc349b6a736eb347a
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-02-28 20:28:35 -0300

    implementação do main

---
+++ consumo-omdb/main.js
@@ -0,0 +1,37 @@
+class Titulo {
+  constructor(nome, duracao, lancamento, genero, diretor){
+    this.nome = nome;
+    this.duracao = duracao;
+    this.lancamento = lancamento;
+    this.genero = genero;
+    this.diretor = diretor;
+  }
+
+  descreveFilme(){
+    return `Filme: ${this.nome}, lançado em: ${this.lancamento},dirigido por ${this.diretor}`;
+  }
+}
+
+let filmes = [];
+
+function buscarDados() {
+  let titulo = document.getElementById('titulo').value;
+  let url = `https://www.omdbapi.com/?t=${titulo}&apikey=6585022c`;
+  alert(url);
+            
+  fetch(url).then(response => {
+    return response.json()})
+    .then(filme => {
+      atribuirDados(filme);
+    });
+}
+
+function atribuirDados(dados){
+  let filme = new Titulo(dados.Title, dados.Runtime, dados.Year,
+    dados.Genre, dados.Director);
+
+  filmes.push(filme)  
+  //alert(JSON.stringify(filme));  
+  alert(filme.descreveFilme());
+
+}
